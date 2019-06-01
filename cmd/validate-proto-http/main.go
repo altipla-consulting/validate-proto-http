d4079504184a817e8f1519d352203a740a9954e9
Ernesto Alejo <ernestokarim@gmail.com>
2019-06-01 13:15:52 +0200

    Walk the folders searching for methods and extract the options.
    No validation performed yet.

--- cmd/validate-proto-http/main.go
+++ cmd/validate-proto-http/main.go
@@ -1,18 +1,92 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/genproto/googleapis/api/annotations"
+	"libs.altipla.consulting/errors"
 )
 
 func main() {
 	if err := run(); err != nil {
-		log.Fatal(err)
+		log.Fatal(errors.Stack(err))
 	}
 }
 
 func run() error {
-	fmt.Println("Hello world!")
+	if os.Getenv("BUILD_ID") != "" {
+		// We don't need colors in Jenkins
+		log.SetFormatter(&log.TextFormatter{DisableColors: true})
+	}
+
+	log.Info("Searching for services to validate")
+	fn := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return errors.Trace(err)
+		}
+
+		if info.IsDir() || filepath.Ext(path) != ".pb" {
+			return nil
+		}
+
+		content, err := ioutil.ReadFile(path)
+		if err != nil {
+			return errors.Trace(err)
+		}
+
+		files := new(descriptor.FileDescriptorSet)
+		if err := proto.Unmarshal(content, files); err != nil {
+			return errors.Trace(err)
+		}
+
+		log.WithField("path", path).Info("Validate services in file")
+
+		for _, file := range files.File {
+			for _, service := range file.Service {
+				log.WithFields(log.Fields{
+					"service": service.GetName(),
+					"file":    file.GetName(),
+				}).Info("... validate service")
+
+				for _, method := range service.GetMethod() {
+					ext, err := proto.GetExtension(method.GetOptions(), annotations.E_Http)
+					if err != nil {
+						return errors.Trace(err)
+					}
+					rule := ext.(*annotations.HttpRule)
+
+					var path string
+					switch r := rule.Pattern.(type) {
+					case *annotations.HttpRule_Get:
+						path = r.Get
+					case *annotations.HttpRule_Put:
+						path = r.Put
+					case *annotations.HttpRule_Post:
+						path = r.Post
+					case *annotations.HttpRule_Delete:
+						path = r.Delete
+					default:
+						return errors.Errorf("unknown http rule pattern: %#v", rule.Pattern)
+					}
+
+					log.WithFields(log.Fields{
+						"name": method.GetName(),
+						"path": path,
+					}).Info("...... validate method")
+				}
+			}
+		}
+
+		return nil
+	}
+	if err := filepath.Walk(".", fn); err != nil {
+		return errors.Trace(err)
+	}
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/altipla-consulting/validate-proto-http
 
 go 1.12
 
-require github.com/sirupsen/logrus v1.4.2
+require (
+	github.com/golang/protobuf v1.3.1
+	github.com/sirupsen/logrus v1.4.2
+	google.golang.org/genproto v0.0.0-20190508193815-b515fa19cec8
+	libs.altipla.consulting v1.26.0
+)

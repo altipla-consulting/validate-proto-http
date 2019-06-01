126c524a53baf7b7af47da5001ca303b9aa59625
Ernesto Alejo <ernestokarim@gmail.com>
2019-06-01 14:45:33 +0200

    Fixes in the install script and instructions.

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Validate gRPC HTTP options mapping the methods to JSON.
 ### Install
 
 ```
-curl -sS https://raw.githubusercontent.com/altipla-consulting/validate-proto-http/master/install.sh | sudo bash
+curl -sSL https://git.io/fjE3d | sudo bash
 ```
--- install.sh
+++ install.sh
@@ -6,10 +6,16 @@ REPO_NAME=validate-proto-http
 
 URL=$(curl -sSL https://api.github.com/repos/altipla-consulting/$REPO_NAME/releases/latest | jq -r '.assets[].browser_download_url')
 echo "Downloading latest release from: $URL"
-curl $URL -sS -o /usr/local/bin/$REPO_NAME || (
+curl $URL -sSL -o /usr/local/bin/$REPO_NAME || (
   echo ""
   echo " [!!!] This command needs sudo priviledge"
   echo ""
   exit 1
 )
-chmod +x /usr/local/bin/$REPO_NAME
+
+chmod +x,o+x /usr/local/bin/$REPO_NAME
+
+echo ""
+echo " > Usage:"
+echo " > validate-proto-http -h"
+echo ""

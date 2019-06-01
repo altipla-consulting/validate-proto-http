d7bfa337847d6bd5cb5b2be2b5d95e2c47eddaf5
Ernesto Alejo <ernestokarim@gmail.com>
2019-06-01 14:41:23 +0200

    Add install instructions.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 # validate-proto-http
 
 Validate gRPC HTTP options mapping the methods to JSON.
+
+## Install
+
+```
+curl -sS https://raw.githubusercontent.com/altipla-consulting/validate-proto-http/master/install.sh | sudo bash
+```
--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 set -eu
 
-URL=$(curl -sSL https://api.github.com/repos/altipla-consulting/validate-proto-http/releases/latest | jq -r '.assets[].browser_download_url')
+REPO_NAME=validate-proto-http
+
+URL=$(curl -sSL https://api.github.com/repos/altipla-consulting/$REPO_NAME/releases/latest | jq -r '.assets[].browser_download_url')
 echo "Downloading latest release from: $URL"
-curl $URL -sS -o /usr/local/bin/validate-proto-http || (
+curl $URL -sS -o /usr/local/bin/$REPO_NAME || (
   echo ""
   echo " [!!!] This command needs sudo priviledge"
   echo ""
   exit 1
 )
+chmod +x /usr/local/bin/$REPO_NAME
